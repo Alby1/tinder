8a85f821693505fcdd1c5ffb886c8cee8b0d164a
schiavoanto <scalarino04@gmail.com>
2023-05-08 22:19:11 +0200

    Initial commit for the website

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Navbar from "../lib/Navbar.svelte";
+</script>
+
+<title>Tinder for Piffy&trade;</title>
+<Navbar selectedPage="Home"></Navbar>
---
+++ web/src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Navbar from "../../lib/Navbar.svelte";
+</script>
+
+<title>About Tinder for Piffy&trade;</title>
+<Navbar selectedPage={"About"}></Navbar>
+<h1>Tinder for Piffy&trade;</h1>
+<p>This is the final IT assignment of this year.</p>
