e2e0f71f699384d6053fc09122f7c1ec05442075
Alby1 <alby.macchi2004@gmail.com>
2023-05-09 11:57:16 +0200

    *voce a tono alto* scarpy

--- crud.py
+++ crud.py
@@ -2,10 +2,6 @@ import db
 import models
 
 import uuid
-from cassandra.cqlengine.management import sync_table
-
-session = db.get_session()
-sync_table(models.Utente)
 
 def create_entry(data: dict) -> models.Utente:
     data['id'] = str(uuid.uuid1())
